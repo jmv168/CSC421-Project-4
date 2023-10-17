d6421e89accee143508c0f27e3f3d6b7efe2caa0
Ubuntu <ubuntu@ip-172-31-12-106.us-east-2.compute.internal>
2023-10-17 04:32:10 +0000

    Update One

--- snail.c
+++ snail.c
@@ -19,6 +19,7 @@ int is_verbose_g = 0 ;
 struct Board board_g ;
 
 void * draw_thread(void * the_args) {
+	//printf("Start of draw_thread");
 	struct T_arg * t_arg = (struct T_arg *) the_args ;
 	struct timeval tv;
     	struct timespec ts;
@@ -28,9 +29,9 @@ void * draw_thread(void * the_args) {
 	board_string = (char *) malloc(BOARD_SIZE+1) ;
 	assert(board_string) ;
 
-	// inside the draw_thread function
 
 	while (1) {
+	   //printf("Draw_thread loop entering");
 	    pthread_mutex_lock(t_arg->mutex) ;
 	    {
 	        if (t_arg->req_exit) {
@@ -38,32 +39,32 @@ void * draw_thread(void * the_args) {
 	            break;
 	        }
 	        
-	        if (snail_collide(&board_g)) {
+	        if (snail_collide(&board_g)) {                     // If snail crashes with the gate, print it, and game over 
 	            if (board_g.gate_state == GATE_IS_CLOSED) {
-	                // Game over because the snail hit the closed gate
-	                printf("Game Over! Snail hit the closed gate.\n");
-	                break;
+	                printf("Game Over! Snail has tragically crashed!\n");
+			exit(0);
 	            }
 	        }
 	        
-	        if (board_gameover(&board_g)) {
-	            printf("Game Over! Snail passed through.\n");
-	            break;
+	        if (board_gameover(&board_g)) {                // If the game is over and snail passed, print it, break out of loop and game is over
+	            printf("Success! Snail has made it through!\n");
+		    exit(0);
 	        }
 	        
-	        if (board_g.guess == board_g.gate_key) {
+	        if (board_g.guess == board_g.gate_key) {   // If the user's guess matches the key, open the gate
 	            board_g.gate_state = GATE_IS_OPEN;
 	        }
 	        
 	        board_draw(board_string, &board_g);
 	        printf("%s\n", board_string);
+
 	        board_g.snail_loc++;
 	        
 	        sleep(SLEEP_TIME);
 	    }   
 	    pthread_mutex_unlock(t_arg->mutex) ;
 	}
-
+	
 
 	pthread_exit(NULL) ;
 	assert(0==1) ; /* never gets here */
@@ -71,6 +72,7 @@ void * draw_thread(void * the_args) {
 
 
 int main(int argc, char * argv[]) {
+	//printf("Start of main function");
 	int ch ;
 	pthread_t thread_id ;
 	char buf[LINE_MAX] ;
@@ -104,8 +106,9 @@ int main(int argc, char * argv[]) {
 	t_arg.cond = &cond;
 	
 	pthread_create(&thread_id, NULL, draw_thread, (void *) &t_arg);
-	
+
 	while (fgets(buf,LINE_MAX,stdin) != NULL) {
+	    //printf("Entering main while loop");
 	    buf[strcspn(buf, "\n")] = 0; // removing newline character
 	    
 	    if (strcmp(buf, QUIT_STRING) == 0) {
@@ -115,7 +118,10 @@ int main(int argc, char * argv[]) {
 	    pthread_mutex_lock(&mutex);
 	    board_g.guess = buf[0]; // Assuming first character of input is the guess
 	    pthread_mutex_unlock(&mutex);
+            
 	}
+	//printf("Outside of main while loop");
+
 	
 	t_arg.req_exit = 1;
 //////
